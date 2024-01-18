681b3e3e0813384c53aba9898a9b7c623b205b0c
dev0@nomo <dev0@nomo.app>
2024-01-18 18:43:57 +0100

    added auto update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import MintingPage from "./app/minting/logic/MintingPage";
 import "./app/globals.css";
 import ClaimRewardsPage from "./app/claiming/logic/ClaimRewardsPage";
 import { HashRouter } from "react-router-dom";
+import { nomo } from "nomo-webon-kit";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    nomo.registerOnWebOnVisible((_args: { cardMode: boolean }) => {
+      nomo.checkForWebOnUpdate();
+    });
+  }, []);
+
   return (
     <HashRouter>
       <Routes>
